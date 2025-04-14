421c5926990849116d898c9bc063545a48d5c84e
Promise278 <promiseobi2007@gmail.com>
2025-04-14 13:55:27 +0100

    added files

---
+++ script.js
@@ -0,0 +1,40 @@
+let adashi = [];
+
+
+function addUsers(name,age,gender,amount) {
+    let newUser = {
+        name: name,
+        age: age,
+        gender: gender,
+        amount: amount,
+    }
+    adashi.push(newUser)
+    console.log("users added to the adashi business", adashi);
+}
+
+addUsers("promise", 32, 'male', '$0')
+addUsers("phebe", 22, 'female', '$0')
+addUsers("timothy", 52, 'male', '$0')
+addUsers("grace", 42, 'female', '$0')
+addUsers("pius", 72, 'male', '$0')
+addUsers("philip", 12, 'male', '$0')
+
+function deleteUser() {
+    let removedUser = adashi.splice(1, 1);
+    console.log("user deleted from the database", removedUser)
+    console.log("after removing a user from the database", adashi)
+}
+
+deleteUser(adashi)
+
+
+
+// function updateUser(amount) {
+//     if (amount === 0) {
+//         console.log("$100 have been added to your account", amount)
+//     } else {
+//         console.log("we will added it for you later", amount)
+//     }
+// }
+// updateUser()
+
