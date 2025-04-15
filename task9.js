601f7be6ab1052ff9051a65c57c364a1a4e4c757
Promise278 <promiseobi2007@gmail.com>
2025-04-15 08:20:24 +0100

    added changes

--- task9.js
+++ task9.js
@@ -1,9 +1,18 @@
 // 9 merge 2 arrys and sort them?
 
-let input = [0, 1, 4, 8 ]
-let output = [2, 3, 5, 6 ]
+// let input = [0, 1, 4, 8 ]
+// let output = [2, 3, 5, 6 ]
 
-let result = input.concat(output)
-result.sort((a, b) => a- b)
+
+// let result = input.concat(output)
+// result.sort((a, b) => a- b)
+
+// console.log(result)
+
+
+let input = [0, 1, 4, 8 ]
+let output = [,2, 3, 5, 6 ]
+
+let result = input + output
 
 console.log(result)
